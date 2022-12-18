7d2fe4e2cb215cac710c4e5ce39c1afb34e60b36
PySavant <savant.pydev@outlook.com>
2022-12-18 01:36:08 -0500

    testing

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
+import asyncio
+
 from app import Sortify
 from app.logger import Config
 
 
 
 logger = Config.setLogger()
-
-
 app = Sortify(logger)
+
+
+async def main():
+    await app.download_library()
+    await app.update_library()
+    await app.generate()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
