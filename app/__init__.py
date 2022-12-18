81c7f25b3d978ebe43319c6489739b7558819f3a
PySavant <savant.pydev@outlook.com>
2022-12-17 22:36:08 -0500

    App Environment Variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,20 @@
+import os
+import dotenv
+import logging
 
+from spotify.oauth import SpotifyOAuth
+from spotify.client import SpotifyClient
+
+
+dotenv.load_dotenv()
+logger = logging.getLogger("sortify")
 
 
 class Sortify():
-    
+
+    id: str = os.getenv('SPOTIFY_CLIENT_ID')
+    secret: str = os.getenv('SPOTIFY_CLIENT_SECRET')
+    redirect: str = os.getenv('SPOTIFY_REDIRECT_URI')
+
+    def __init__(self):
+        self.auth = SpotifyClient(self)
